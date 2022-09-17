c18fe28b3d1abcbd5599887a6965f29b0248c237
413831 <alepleins3pp@gmail.com>
2022-09-17 20:27:39 -0300

    Init server NodeJS

---
+++ ticket-app/api/app.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const app = express();
+mongodb = require("mongodb");
+cliente = mongodb.MongoClient; //En cliente ya tenemos nuestro acceso a la BD.
+
+const uri = "mongodb+srv://bddosutn:<passoword>@cluster0.bbrw0lt.mongodb.net/?retryWrites=true&w=majority"
+
+cliente.connect(uri, (err, client) => 
+{ 
+    if(err) 
+    {
+        console.log("Error: ", err);
+        return; 
+    }
+    // Instancia de Cliente de MongoDB
+    //console.log(cliente); 
+    
+    app.use((req, res, next) => {
+        console.log("middleware");
+        next();
+    }) 
+
+    let tickets = client.db('TP_BD_II').collection("tickets");
+    let clientes = client.db('TP_BD_II').collection("clientes");
+    let empleados = client.db('TP_BD_II').collection("empleados");
+    let equipos = client.db('TP_BD_II').collection("equipos");
+
+    // TICKETS
+    app.get("/tickets", (req, res) => { 
+        res.json(getAll(tickets)), 
+        err => { if(err) console.log(err) }
+    });
+        //res.json(result); //No puedo tener 2 respuestas.
+
+    app.get("/tickets/:id", (req, res) => {
+        console.log(req);
+        res.json(get(tickets, req.params.id))
+        , err => {
+            if(err){
+                console.log(err);
+
+            }
+            res.json(datos);
+    }})
+        //res.json({rta: "Hola"}); //No puedo tener 2 respuestas.
+
+
+    app.get("/usuarios/agregar", (req, res) => {
+        //let usu = JSON.parse(req.params.usuario);
+        let usu = usuarios.insert({usuario: "pepe", email: "pepe@pepe.com", clave: "123", foto: "miFoto.jpg"});
+        res.json(usu);
+    });
+
+    app.get("/usuarios/listar/:nombre", (req, res) => {
+        let arrayUsu = [];
+        let nombre = req.params.nombre;
+        usuarios.find({usuario: nombre}).forEach(item => {
+            arrayUsu.push(item);
+        }, err => {
+            if(err){
+                console.log(err);
+            }
+            res.json(arrayUsu);
+        })
+    });
+
+    app.get("/listado/:persona/:apellido", (req, res) => {
+        console.log(req.params.persona, req.params.apellido);
+        res.json({rta: "Hola"});
+    });
+
+    /*app.post("/", (req, res) => {
+        res.json({rta: "Hola"});
+    });*/
+
+    app.listen(3000, () => {
+        console.log("Servidor escuchando en puerto 3000");
+    });
+})
+
+// Handler methods
+
+function getAll(collection){
+    let datos = [];
+
+    collection.find().forEach( item => { datos.push(item) });
+    return datos;
+}
+
+function get(collection, filter){
+    let datos = [];
+
+    collection.find({filter}).forEach(item => { datos.push(item)});
+    return datos;
+}
